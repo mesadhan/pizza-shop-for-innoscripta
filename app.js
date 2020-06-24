1640babb5e75eca35f6fa464ffecb15083f821b3
Md. Sadhan Sarker <sadhansarker3@gmail.com>
2020-06-24 13:05:14 +0600

    setup DB connection

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const  express = require('express');
 const cors = require('cors');
+const db = require('./config/database');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const routePrefix = '/api';
+
+
+// Test DB Connection
+
+db.authenticate()
+    .then(() => console.log('[message]', 'database connected successfully'))
+    .catch(error => console.log('[error]: ' + error));
 
 
 // default routes

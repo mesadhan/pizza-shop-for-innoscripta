a1eeea05e9d6941f493731bd7440f9f7e6728a2b
Md. Sadhan Sarker <sadhansarker3@gmail.com>
2020-06-25 00:22:21 +0600

    expose public static content expose globally

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const db = require('./config/database');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'))
+app.use('/static', express.static('public'))   // access 'public' dir contents using 'static' route
 
 const routePrefix = '/api';
 
 
 // Test DB Connection
-
 db.authenticate()
     .then(() => console.log('[message]', 'database connected successfully'))
     .catch(error => console.log('[error]: ' + error));
@@ -22,6 +23,7 @@ app.get(`/`, async (req, res) => res.status(200).json({ message: 'Hello! from Pi
 
 // sub-routes
 const UserRoutes = require('./routes/users');
+const PizzasRoutes = require('./routes/pizzas');
 
 
 
@@ -29,6 +31,7 @@ const UserRoutes = require('./routes/users');
 
 // routes
 app.use(`${routePrefix}/users`, UserRoutes);
+app.use(`${routePrefix}/pizzas`, PizzasRoutes);
 
 
 

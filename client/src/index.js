5bb257caea6fdb452122dfaf13653b4a47e56a3a
Md. Sadhan Sarker <sadhansarker3@gmail.com>
2020-06-24 14:48:20 +0600

    setup logo & redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from "react-redux";
+import { BrowserRouter as Router} from 'react-router-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+
+// create store
+const initState = {};
+const store = createStore(rootReducer, initState, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router> <App/> </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

c7a7923135ebc0aea05083b0a06cbe7c8ce0110f
Md. Sadhan Sarker <sadhansarker3@gmail.com>
2020-06-24 13:47:32 +0600

    change log

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,console.log(`Server is running under port ${PORT}`))
+app.listen(PORT, console.log(`Server is running at port ${PORT}`))

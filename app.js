2c1ec0f0bf1b52ef597ab76e205641760cd593f0
Md. Sadhan Sarker <sadhansarker3@gmail.com>
2020-06-24 08:22:22 +0600

    project layout & routes updated

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const routePrefix = '/api';
 
 
-app.get('/', async (req, res) => {
-    res.status(200).json({ message: 'Hello! From Node.' });
-});
+// default routes
+app.get(`/`, async (req, res) => res.status(200).json({ message: 'Hello! from Pizza Shop.' }));
+
+
+// sub-routes
+const UserRoutes = require('./routes/users');
+
+
+
+
+
+// routes
+app.use(`${routePrefix}/users`, UserRoutes);
+
+
+
+
+
 
 module.exports = app;

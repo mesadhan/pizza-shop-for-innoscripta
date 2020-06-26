dfbd94997326591379bf36be39c71ed1b315bf13
Md. Sadhan Sarker <sadhansarker3@gmail.com>
2020-06-26 16:13:58 +0600

    added euro and dollar both

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -5,7 +5,7 @@ export const fetchPizzas = () => async dispatch => {
     //todo: need to bind with backend.
     const res = {
         data: [
-            {id: 1, name: "title", description: "Description", price: "22", qnt: 1, img: "https://www.delonghi.com/Global/recipes/multifry/97.jpg", createdAt: "", updatedAt: ""},
+            {id: 1, name: "This title is long for testing", description: "Description", price: "22", qnt: 1, img: "https://www.delonghi.com/Global/recipes/multifry/97.jpg", createdAt: "", updatedAt: ""},
             {id: 2, name: "title", description: "Description", price: "22", qnt: 1, img: "https://www.delonghi.com/Global/recipes/multifry/97.jpg", createdAt: "", updatedAt: ""},
             {id: 3, name: "title", description: "Description", price: "22", qnt: 1, img: "https://www.delonghi.com/Global/recipes/multifry/97.jpg", createdAt: "", updatedAt: ""},
             {id: 4, name: "title", description: "Description", price: "22", qnt: 1, img: "https://www.delonghi.com/Global/recipes/multifry/97.jpg", createdAt: "", updatedAt: ""},
--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -6,17 +6,20 @@ import {removeFromCart, increaseQuantity, decreaseQuantity} from '../actions/car
 class CartItem extends Component {
 
     render() {
-        let {id, name, price, img, qnt} = this.props;
+        let {id, name, img, price, qnt} = this.props
+        let euroPrice = price
+        let dollarPrice = price *  1.12;        // Here, 1 EURO = 1.12 Dollar
+        dollarPrice = dollarPrice.toFixed(2);
 
         return (
             <div data-test="cartItemComponent" className="cartItemComponent">
 
                 <div className="cart-item-container">
                     <div className="cart-item" >
-                        <img src={img} className="" style={{width: '80px'}} alt={name} />
+                        <img src={img} className="" style={{width: '40px'}} alt={name} />
                     </div>
                     <div className="cart-item">{name}</div>
-                    <div className="cart-item">{price}</div>
+                    <div className="cart-item">Price: {euroPrice} ERO <br/> ${dollarPrice} USD</div>
                     <button  className="cart-remove" onClick={()=>this.props.removeFromCart(this.props.cartItems,{id: id})} >X</button>
                 </div>
 
